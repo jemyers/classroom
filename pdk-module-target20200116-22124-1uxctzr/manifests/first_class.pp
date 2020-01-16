0288373f8dc8b638afb3c432c05bb46c437c3205
Jeremy Myers <jeremy@Jeremys-MacBook-Pro.local>
2020-01-16 14:55:00 -0500

    adding module

---
+++ pdk-module-target20200116-22124-1uxctzr/manifests/first_class.pp
@@ -0,0 +1,8 @@
+# @summary A short summary of the purpose of this class
+#
+# A description of what this class does
+#
+# @example
+#   include classroom::first_class
+class classroom::first_class {
+}
