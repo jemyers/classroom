7cde1cf5ad422568f115bad3089ff7a04a2df809
Jeremy Myers <jeremy@Jeremys-MacBook-Pro.local>
2020-04-19 12:58:29 -0400

    add

--- site-modules/profile/manifests/archive.pp
+++ site-modules/profile/manifests/archive.pp
@@ -1,8 +1,10 @@
-archive { '/tmp/test.txt':
+class profile::archive { 'archive':
+  archive { '/tmp/test.txt':
   ensure        => present,
   extract       => true,
   extract_path  => '/tmp',
   source        => 'http://central.maven.org/maven2/javax/transaction/jta/1.1/jta-1.1.jar',
   creates       => '/tmp/javax',
   cleanup       => false,
+ }
 }
