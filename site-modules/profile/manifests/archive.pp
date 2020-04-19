d491166039d0bc4a6bd39e073eeea7a5b5020933
Jeremy Myers <jeremy@Jeremys-MacBook-Pro.local>
2020-04-19 12:53:51 -0400

    add archive

---
+++ site-modules/profile/manifests/archive.pp
@@ -0,0 +1,8 @@
+archive { '/tmp/test.txt':
+  ensure        => present,
+  extract       => true,
+  extract_path  => '/tmp',
+  source        => 'http://central.maven.org/maven2/javax/transaction/jta/1.1/jta-1.1.jar',
+  creates       => '/tmp/javax',
+  cleanup       => false,
+}
