b428810bc2200b39f52cfe0b50f8cfbc70a1ee83
Jeremy Myers <jeremy@Jeremys-MacBook-Pro.local>
2020-04-19 13:00:54 -0400

    edit

--- site-modules/profile/manifests/archive.pp
+++ site-modules/profile/manifests/archive.pp
@@ -1,10 +1,10 @@
 class profile::archive { 'archive':
   archive { '/tmp/test.txt':
-  ensure        => present,
-  extract       => true,
-  extract_path  => '/tmp',
-  source        => 'http://central.maven.org/maven2/javax/transaction/jta/1.1/jta-1.1.jar',
-  creates       => '/tmp/javax',
-  cleanup       => false,
- }
+  ensure       => present,
+  extract      => true,
+  extract_path => '/tmp',
+  source       => 'http://central.maven.org/maven2/javax/transaction/jta/1.1/jta-1.1.jar',
+  creates      => '/tmp/javax',
+  cleanup      => false,
+  }
 }
