1bf607179c2afef3b5ec221a63f413f970f8f6c3
Jeremy Myers <jeremy@Jeremys-MacBook-Pro.local>
2020-04-19 14:06:23 -0400

    edit archive

--- site-modules/profile/manifests/archive.pp
+++ site-modules/profile/manifests/archive.pp
@@ -1,7 +1,9 @@
+
+
 class profile::archive {
   archive { '/tmp/jta-1.1.jar':
   ensure       => present,
-  extract      => false,
+  extract      => true,
   extract_path => '/tmp',
   source       => 'http://central.maven.org/maven2/javax/transaction/jta/1.1/jta-1.1.jar',
   creates      => '/tmp/javax',
