73dd94087cf61a6ef2c0b098b0f887caec164285
Jeremy Myers <jeremy@Jeremys-MacBook-Pro.local>
2020-04-20 10:38:37 -0400

    adding heira data

--- site-modules/profile/manifests/archive.pp
+++ site-modules/profile/manifests/archive.pp
@@ -1,10 +1,18 @@
-class profile::archive {
-  archive { '/tmp/test.tar.gz':
+class profile::archive (
+  String $filename    # version being downloaded
+ # String $created_path # Directory containg the war files
+
+) {
+
+#  $artifactory_host = heira('artifactory_host')
+#  $artifactory_port = heira('artifactroy_port')
+
+  archive { "/tmp/${filename}":
   ensure       => present,
-  extract      => false,
+  extract      => true,
   extract_path => '/tmp',
-  source       => 'https://downloads.apache.org/tomcat/tomcat-9/v9.0.33/bin/apache-tomcat-9.0.33-deployer.tar.gz',
+  source       => "https://downloads.apache.org/tomcat/tomcat-9/v9.0.33/bin/apache-tomcat-9.0.33-deployer.tar.gz",
   creates      => '/tmp/javax',
-  cleanup      => false,
+  cleanup      => true,
   }
 }
